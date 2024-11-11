6170a6f47736f1ff7fea8d8a364560d8cd908ed2
Tu Nombre <you@example.com>
2024-11-11 09:38:43 -0300

    http module error arreglado

--- src/app/agregarcasa/agregarcasa.component.spec.ts
+++ src/app/agregarcasa/agregarcasa.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AgregarcasaComponent } from './agregarcasa.component';
 
 describe('AgregarcasaComponent', () => {
@@ -8,7 +8,7 @@ describe('AgregarcasaComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AgregarcasaComponent]
+      imports: [AgregarcasaComponent, HttpClientModule]
     })
     .compileComponents();
     
