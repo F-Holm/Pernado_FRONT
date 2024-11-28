e1c7fa82e88dc189d8091048460664f5475e061c
Holm <you@example.com>
2024-11-27 22:45:15 -0300

    funciona filtros y busqueda

--- src/app/services/propiedad-api.service.ts
+++ src/app/services/propiedad-api.service.ts
@@ -23,16 +23,16 @@ export class PropiedadApiService {
     return (this.http.get<any>(this.BASE_URL + "/usuario/" + idUsuario));
   }
 
-  getPropiedadesFiltros(filtrosPropiedad: IFiltrosPropiedad): any {
-    return (this.http.get<any>(this.BASE_URL + '/filtered/' + JSON.stringify(filtrosPropiedad)));
+  getPropiedadesFiltros(query: any): any {
+    return (this.http.post<any>(this.BASE_URL + '/filtered', { 'query' : query }));
   }
 
   getPropiedadesLimitSkip(limit: number, skip: number): any {
     return (this.http.get<any>(this.BASE_URL + '/ls/' + limit + '/' + skip));
   }
 
-  getPropiedadesFiltrosLimitSkip(filtrosPropiedad: IFiltrosPropiedad, limit: number, skip: number): any {
-    return (this.http.get<any>(this.BASE_URL + '/fileterls/' + JSON.stringify(filtrosPropiedad) + '/' + limit + '/' + skip));
+  getPropiedadesFiltrosLimitSkip(query: any, limit: number, skip: number): any {
+    return (this.http.post<any>(this.BASE_URL + '/filteredls', { "query": query, "limit": limit, "skip": skip }));
   }
 
   getPropiedad(id: number): any {
